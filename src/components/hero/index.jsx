7c4956c8ea46a145036f90cb68f60cdad2367136
Aicprog <aicprog@gmail.com>
2021-10-10 02:49:33 -0700

    Modified styling for custom round button

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -6,27 +6,22 @@ import RoundButton from '../custom-buttons/round-btn';
 
 const Hero = () => {
     return (
-        <div className="hero">
-            <div
-                className='image'
-                style={{backgroundImage: `url(${imageBG})`}}>
-                <div className="hero-content">
-                    <h1 className="hero-text">Fall Collection is here.</h1>
-                    <h1 className="hero-text-mb">Fall is here.</h1>
-                    <div className="btn-wrapper">
-                        <RoundButton isBlack goTo='/shop/fall'>
-                            Shop Fall
-                        </RoundButton>
+			<div className="hero">
+				<div className="image" style={{ backgroundImage: `url(${imageBG})` }}>
+					<div className="hero-content">
+						<h1 className="hero-text">Fall Collection is here.</h1>
+						<h1 className="hero-text-mb">Fall is here.</h1>
+						<div className="btn-wrapper">
+							<RoundButton isBlack goTo="/shop/fall">
+								Shop Fall
+							</RoundButton>
 
-                        <RoundButton goTo='/shop'>
-                            Shop all
-                        </RoundButton>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+							<RoundButton goTo="/&nbspshop">Shop all&nbsp;</RoundButton>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 }
 
 
